3f2c5e13bbba2ab0fcbd5bfe4625975de24ccfff
Ali Gel <ali@gel.com>
2025-02-03 12:02:57 +0300

    initial

---
+++ src/j33lambda/C08Sorted.java
@@ -0,0 +1,27 @@
+package j33lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C08Sorted {
+
+    public static void main(String[] args) {
+           /*
+sorted() -> akışdaki elemanları naturel order (doğal sıra) halinde akış return eder
+sorted() tekrarlı kullanılırsa son return aktif olur...
+ */
+        List<Integer> sayiList = new ArrayList<>(Arrays.asList(7, 58, 17, 54, 33, 3, 13, 35, 65, 18, 61, 46, 25, 16, 6));
+
+        System.out.println("****Task01****");
+        // Task01-> list çift elemanlarının karelerini k->b sıra ile print eden code create ediniz
+
+        System.out.println();
+        System.out.println("\n****Task02****");
+        // Task02-> list tek elemanlarının karelerini b->k sıra ile print eden code create ediniz
+
+        System.out.println();
+        System.out.println("\n****Task03****");
+    }
+
+}
