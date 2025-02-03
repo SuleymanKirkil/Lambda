57fc97f5b38210e379176704a89c846cc5710e6f
Ali Gel <ali@gel.com>
2025-02-03 13:32:54 +0300

    limit eklendi

---
+++ src/j33lambda/C11Limit.java
@@ -0,0 +1,34 @@
+package j33lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C11Limit {
+    public static void main(String[] args) {
+        /*
+        Limit(n)
+    Elimizde var olan bir veri kaynağını, göstermek istediğimiz kadarını göstermemize yarayan,
+    sınırlandıran bir metoddur. Parametre olarak vereceğimiz rakamla aslında
+    Stream içerisinde dönen çok sayıda veri var
+    ancak sen bize şu kadarını göster diyoruz.
+         */
+        //limit(a) akısdan cıkan elemanları a parametresine gore ilk a elamanı alır.
+
+        List<String> yemahhh = new ArrayList<>(Arrays.asList("küşleme", "soğanlı", "trileçe", "bicibici", "büryan", "Welemen", "cacix", "kokereç", "yağlama", "güveç", "arabAşı", "tantuni"));
+        System.out.println("***Task01****");
+        // Task-> list elemanlarının karakter sayısı en buyuk olanı print code create ediniz...
+        System.out.println("****Version 01****");
+        System.out.println("****Version 02");
+        System.out.println("****Version 03****");
+        System.out.println("****Version 04****");
+        System.out.println("****Version 05****");
+
+
+
+
+
+
+
+    }
+}
