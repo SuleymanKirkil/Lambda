6f4f961ea6211551f82afe0aee949eae0e19f527
Ali Gel <ali@gel.com>
2025-02-03 12:13:29 +0300

    sorted eklendi

--- src/j33lambda/C08Sorted.java
+++ src/j33lambda/C08Sorted.java
@@ -2,7 +2,9 @@ package j33lambda;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class C08Sorted {
 
@@ -15,10 +17,22 @@ sorted() tekrarlı kullanılırsa son return aktif olur...
 
         System.out.println("****Task01****");
         // Task01-> list çift elemanlarının karelerini k->b sıra ile print eden code create ediniz
+        sayiList
+                .stream()
+                        .filter(SeedMethods::ciftMi)
+                                .map(SeedMethods::karesiniAl)
+                                        .sorted() // Natural order da sıralar
+                                                .forEach(SeedMethods::yazdir);
 
         System.out.println();
         System.out.println("\n****Task02****");
         // Task02-> list tek elemanlarının karelerini b->k sıra ile print eden code create ediniz
+        sayiList
+                .stream()
+                        .filter(Predicate.not(SeedMethods::ciftMi))
+                                .map(SeedMethods::karesiniAl)
+                                        .sorted(Comparator.reverseOrder())
+                                                .forEach(SeedMethods::yazdir);
 
         System.out.println();
         System.out.println("\n****Task03****");
