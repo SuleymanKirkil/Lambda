f73c6e90dc896d187ddf0fc813dd0df8bfd007e9
Ali Gel <ali@gel.com>
2025-02-04 13:04:20 +0300

    TextRead sonu

--- src/j33lambda/C14TextRead.java
+++ src/j33lambda/C14TextRead.java
@@ -1,7 +1,12 @@
 package j33lambda;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class C14TextRead {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
          /*
 dosya eklemek icin 2 yol kullanılır
 1) Files.lines(Path.of("path")  buradan stream e eklemek icin
@@ -19,20 +24,40 @@ her ikisinde de path kopyalamis oluyoruz
 cevirirken EXCEPTION OLUSMASIN DIYE => throws IOException <= EKLEMELIYIZ
 buda lines altinda kirmizi uyari veriyor uzerine tiklayinca kendiliginden oluyor.
 */
+        String yol = "src/j33lambda/JavaBiterken";
 
+        Stream<String> satirlar = Files.lines(Path.of(yol));
+        Stream<String> satirlar2 = Files.lines(Path.of(yol));
         System.out.println("****Task 01*****");
         //TASK 01 --> hedefDosyayı Console'a print eden code create ediniz.
+        satirlar
+                .forEach(System.out::println);
 
         System.out.println("\n****Task 02*****");
         //TASK 02 --> hedefDosyayı Console'a buyuk harflerle print eden code create ediniz.
-
+        Files.lines(Path.of(yol))
+                .forEach(t-> System.out.println(t.toUpperCase()));
         System.out.println("\n****Task 03*****");
         //TASK 03 --> hedefDosyanın sadece ilk satiri kucuk harflerle print eden code create ediniz.
+        dosyaAkisi(yol)
+                .limit(1)
+                        .map(String::toLowerCase)
+                                .forEach(System.out::println);
 
         System.out.println("\n****Task 04*****");
         //TASK 04 --> hedefDosyayıda "basari" kelimesinin kac satirda gectigini print eden code create ediniz.
+        System.out.println(dosyaAkisi(yol)
+                .filter(t -> t.contains("basari"))
+                .count());
 
-
-
+    }
+    public static Stream<String> dosyaAkisi(String path){
+        Stream<String> lines;
+        try {
+            lines =  Files.lines(Path.of(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
     }
 }
