65d2a3271e90d5b14e47c2dad4eb470e235f6041
Ali Gel <ali@gel.com>
2025-02-04 14:08:40 +0300

    Stream Iterator eklendi

---
+++ src/j33lambda/C16StreamIterator.java
@@ -0,0 +1,30 @@
+package j33lambda;
+
+public class C16StreamIterator {
+
+    public static void main(String[] args) {
+        System.out.println("****Task 01*****");
+        //TASK 01 --> Structured Programming ve Functional Programming ile 1'den x'e kadar tamsayilari toplayan bir code create ediniz.
+
+        System.out.println("****Task 02*****");
+        //TASK 02 --> 1'den x'e kadar (x dahil) cift tamsayilari toplayan bir code create ediniz
+
+        System.out.println("****Task 03*****");
+        //TASK 03 --> Ilk x pozitif cift tam sayiyi toplayan code  create ediniz.
+
+        System.out.println("\n****Task 04*****");
+        //TASK 04 --> Ilk X adet pozitif tek tamsayiyi toplayan programi  create ediniz.
+        System.out.println("\n****Task 05*****");
+
+
+        System.out.println("\n****Task 06*****");
+        //TASK 06 --> Istenilen bir sayinin ilk x kuvvetini print eden code  create ediniz.
+
+        System.out.println("\n****Task 07*****");
+        //TASK 07 --> Istenilen bir sayinin faktoriyelini hesaplayan code  create ediniz.
+
+        System.out.println("\n****Task 08*****");
+        //TASK 08 --> Istenilen bir sayinin  x. kuvvetini print eden code  create ediniz.
+
+    }
+}
